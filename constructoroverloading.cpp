6586d4db7f70b4987164036da2e25be207da9830
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-04 21:11:52 +0700

    main program mhs nim  dan  nama

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -42,3 +42,10 @@ void mahasiswa::cetak()
     cout << "Nama = " << nama << endl;
 }
 
+int main()
+{
+     mahasiswa mhs1(102030); 
+     mahasiswa mhs2("Andi");   
+     mahasiswa mhs3(010203, "Santi"); 
+}
+
