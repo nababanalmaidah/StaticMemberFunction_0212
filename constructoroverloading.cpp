f95f4217b68f6bc3fe0a491ee731a38fa549e86f
nababanalmaidah <nababanalmaidah@gmail.com>
2026-05-04 21:10:33 +0700

    fungsi mahassiswa dan isinya

--- constructoroverloading.cpp
+++ constructoroverloading.cpp
@@ -36,3 +36,9 @@ mahasiswa :: mahasiswa(int iNim, string iNama)
     nama = iNama;
 }
 
+void mahasiswa::cetak() 
+{
+    cout << endl << "Nim = " << nim << endl;
+    cout << "Nama = " << nama << endl;
+}
+
